6be779ab723f92307ffc302875154fd536a425a2
Marlin Rodriguez <marodriguez@microsoft.com>
2025-11-04 09:59:50 -0500

    Fixed prettier errors and removed PowerShell helper script

--- specification/mission/Mission.Management/examples/2025-11-01-preview/update-api-version.ps1
+++
@@ -1,6 +0,0 @@
-$files = Get-ChildItem -Filter "*.json"
-foreach ($file in $files) {
-    $content = Get-Content $file.FullName -Raw
-    $content = $content -replace '"api-version": "2025-09-01-preview"', '"api-version": "2025-11-01-preview"'
-    Set-Content -Path $file.FullName -Value $content
-}
